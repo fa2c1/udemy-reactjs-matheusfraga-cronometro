c516aab986f8174b8ceada26c9a411a6b068fca4
Fernando Coutinho <fa2c1@hotmail.com>
2022-02-19 14:40:57 -0300

    Estrutura e estilização do projeto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
+import cronometro from './assets/cronometro.png'; 
+import './style.css'
 
 class App extends Component {
 
@@ -9,8 +12,13 @@ class App extends Component {
 
   render(){
     return (
-      <div>
-        <h1>Começando o App cronometro</h1>
+      <div className="container">
+        <img className="img" src={cronometro} alt="Cronometro" />
+        <a className="timer">0:0</a>
+        <div className="areaBtn">
+          <a className="botao">VAI</a>
+          <a className="botao">LIMPAR</a>
+        </div>
       </div>
     );
   }
